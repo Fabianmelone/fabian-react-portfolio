8a39ab853159ace569a0558cdc051fa50e1a491b
Fabi <fabianmelone.fw@gmail.com>
2024-11-12 09:45:38 +0100

    Update Footer.jsx
    include new name Fabianmelone

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,7 @@ function Footer () {
     return (
         <footer className="footer fixed-bottom text-center">
             {/* icon links for github, linkedin, instagram and twitter */}
-            <a className="icon-links" href="https://github.com/PythonFabi" aria-label="PythonFabi's Github Profile"><FontAwesomeIcon icon={faGithub} /></a>
+            <a className="icon-links" href="https://github.com/Fabianmelone" aria-label="Fabianmelone's Github Profile"><FontAwesomeIcon icon={faGithub} /></a>
             <a className="icon-links" href="https://www.linkedin.com/in/fabian-wagner-70905a281/" aria-label="Fabian's LinkedIn Profile"><FontAwesomeIcon icon={faLinkedin} /></a>
             <a className="icon-links" href="https://instagram.com/fabi_wag_?igshid=YmMyMTA2M2Y=" aria-label="Fabian's Instagram Profile"><FontAwesomeIcon icon={faInstagram} /></a>
             <a className="icon-links" href="https://twitter.com/fabi_wag_" aria-label="Fabian's Twitter Profile"><FontAwesomeIcon icon={faXTwitter} /></a>
